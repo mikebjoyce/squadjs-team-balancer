edb6fd10b2e05dc17bbdb207340371595d561cf0
Michael Joyce <5810516+mikebjoyce@users.noreply.github.com>
2026-06-05 18:59:02 -0400

    Fixed team balancer not informing switch plugin properly on scramble for scramble lockout

--- plugins/team-balancer.js
+++ plugins/team-balancer.js
@@ -1883,7 +1883,7 @@ export default class TeamBalancer extends BasePlugin {
         Logger.verbose('TeamBalancer', 2, `Dry run: Scrambler returned ${swapPlan.length} player moves (Calculation: ${swapPlan.calculationTime}ms).`);
 
         if (!isSimulated) {          
-          const affectedPlayers = this.server.players.map(p => ({ eosID: p.eosID, name: p.name }));
+          const affectedPlayers = this.server.players.map(p => ({ steamID: p.steamID, name: p.name }));
           this.server.emit('TEAM_BALANCER_SCRAMBLE_EXECUTED', {
             affectedPlayers
           });
